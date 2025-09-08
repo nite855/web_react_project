29d8976df2b522c76547baeb733f8594e4af23e8
Sung JinWoo <wlsdn42710801@gmail.com>
2025-09-08 14:25:12 +0900

    더미데이터 API로 변경, useEffect로 db에 반영

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,17 +1,23 @@
-import dummy from "../db/data.json";
+import { use, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Word from "../component/Word";
 
 export default function Day() {
   const { day } = useParams(); //useParams :URL 파라미터를 받아오는 훅, 문자열을 받아옴
-  const wordList = dummy.words.filter(word => word.day === Number(day)); //문자 숫자 비교니 형변환 필요
+  const [Words, setWords] = useState([]); //Words : 빈 배열로 초기화, setWords : Words 상태를 업데이트하는 함수
+
+ useEffect(() => {
+    fetch(`http://localhost:3001/words?day=${day}`) //fetch : 지정된 URL로 HTTP 요청을 보내고, 응답을 처리하는 함수
+      .then(res => res.json()) //.json() : 응답 객체를 JSON 형식으로 변환하는 메서드, json : JavaScript Object Notation
+      .then(data => setWords(data)); //setWords(data) : 받아온 데이터를 Words 상태로 설정
+  }, [] ); //useEfffect에서 특정값을 사용시 의존성배열, 두번째 인자에 그  값을 넣어줘야함
 
  return (
     <>
       <h2>Day {day}</h2>
       <table>
         <tbody>
-          {wordList.map(word => (
+          {Words.map(word => (
             <Word word={word} key={word.id} />
           ))}
         </tbody>
--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,10 +1,17 @@
 import dummy from '../db/data.json'  //dummy :data.json 파일의 내용을 불러와서 dummy라는 객체에 저장
 import { Link } from "react-router-dom"; //Link : a태그 대신 사용, 페이지 전환 없이도 이동 가능
-
+import { useEffect, useState } from 'react';
 export default function DayList() {
+  const [days, setDays] = useState([]); //days : 빈 배열로 초기화, setDays : days 상태를 업데이트하는 함수
+  
+  useEffect(() => {
+    fetch('http://localhost:3001/days') //fetch : 지정된 URL로 HTTP 요청을 보내고, 응답을 처리하는 함수
+      .then(res => res.json()) //.json() : 응답 객체를 JSON 형식으로 변환하는 메서드, json : JavaScript Object Notation
+      .then(data => setDays(data)); //setDays(data) : 받아온 데이터를 days 상태로 설정
+  })
   return (
     <ul className="list_day">
-      {dummy.days.map(day => (
+      {days.map(day => (
         <li key={day.id}>
           <Link to={`/day/${day.day}`}>Day {day.day}</Link>
         </li>
@@ -13,4 +20,5 @@ export default function DayList() {
     );
     //화살표 함수에서 return을 생략할 때는 중괄호 대신 소괄호를 사용해야 합니다. 
 }
+
        
